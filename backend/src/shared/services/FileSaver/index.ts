8a42278524368a7feda31e4aada9838d83924eae
Carmen Santiago <greminoficial@gmail.com>
2021-02-02 11:50:35 -0600

    [Add] file storage name env var & docker minio keys and volume

--- backend/src/shared/services/FileSaver/index.ts
+++ backend/src/shared/services/FileSaver/index.ts
@@ -1,24 +1,24 @@
 
 import { makeLocalFileSaver } from './LocalFileSaver'
-import {makeSaveFileS3 } from './S3';
+import { makeSaveFileS3 } from './S3';
 import config from '../../../config'
 
 const {
-  FILE_STORAGE_ACCESS_KEY,
+	FILE_STORAGE_ACCESS_KEY,
 	FILE_STORAGE_SECRET_ACCESS,
 	FILE_STORAGE_ENDPOINT,
+	FILE_STORAGE_NAME
 } = process.env; 
 
 const saveFile = makeLocalFileSaver(config.fileStoragePath);
-const saveFileS3 =  makeSaveFileS3({
-	  accessKey: FILE_STORAGE_ACCESS_KEY,
+const StorageService =  makeSaveFileS3({
+	accessKey: FILE_STORAGE_ACCESS_KEY,
     secretKey: FILE_STORAGE_SECRET_ACCESS, 
-		region: undefined, endpoint: FILE_STORAGE_ENDPOINT,
-		BucketName: 'lower-task-image-bucket'
+	region: undefined, endpoint: FILE_STORAGE_ENDPOINT,
+	BucketName: FILE_STORAGE_NAME
 });
-
 
 export {
     saveFile,
-		saveFileS3
+    StorageService
 }
