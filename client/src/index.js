2f54c5d14a192a0ad98dd6348761ba82b6fa4f06
Carlos Pascacio <carlossanpas@outlook.com>
2021-02-16 09:45:04 -0600

    Sign In & Sign Up Screens created. Styles created too

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { App } from './modules/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import "./global.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
