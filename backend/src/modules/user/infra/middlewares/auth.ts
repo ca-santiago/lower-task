bc425029eae2119ea103c35cfc63639b1fa7376b
Carmen Santiago <greminoficial@gmail.com>
2021-02-22 23:17:36 -0600

    [Add] Integrated auth-service as an authentication middleware

---
+++ backend/src/modules/user/infra/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { verifyCredentials as vc } from '../../services/auth';
+
+export async function authenticateRequest (req, res, next) {
+    const tokenOrNull = analizeToken(
+        req.headers["authorization"] ||
+        req.headers["token"]
+    );
+    const refreshOrNull = req.headers["refreshtoken"];
+
+    if(!tokenOrNull || !refreshOrNull)
+        return res.status(401).end();
+
+    const result = await vc(tokenOrNull, refreshOrNull);
+    if(result.isSuccess === false)
+        return res.status(401).end();
+
+    const { token, refreshToken, accountId } = result.getValue();
+    req.body.token = token;
+    req.body.refreshToken = refreshToken;
+    req.body.accountId = accountId;
+    next();
+}
+
+
+export function analizeToken(maybe: any): string | null {
+  if (typeof maybe !== "string") return null;
+  const payload: string[] = maybe.split(" ");
+
+  return !!payload[1] === false ? null : payload[1];
+}
+
